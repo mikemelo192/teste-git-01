d4ef09f14b0adca7601c53b6cba6eeeb1167254f
Michael Douglas Ferreira de Melo <DouglasMichael@academicos.com>
2025-11-18 20:56:54 -0300

    Criação do arquivo soma

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # teste-git-01
 meu teste de repositório. 
+
+Estou fazendo as primeiras alterações no arquivo de texto Readme.md
---
+++ soma.py
@@ -0,0 +1,6 @@
+a = int(input("Digite um numero: "))
+b = int(input("digite outro numero: "))
+
+soma = a + b
+
+print(f"O resultado da soma dos numeros é: {soma}")
